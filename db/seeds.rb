e469b2908e7bb6cf514f2a532847df9c44d1a8cc
saska petrovic <saskapetrovic.pro@gmail.com>
2024-07-03 15:49:51 +0200

    exercice done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+# List of valid categories
+categories = %w[chinese italian japanese french belgian]
+
+# Create 5 restaurants
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: categories.sample
+  )
+end
+
+puts "Seeded 5 restaurants"
